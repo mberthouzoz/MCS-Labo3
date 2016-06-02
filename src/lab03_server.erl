20bb0190f177f8cd160bcd2931950a49946509c1
David Villa <david.villa@heig-vd.ch>
2016-06-02 11:20:00 +0200

    Recreation du projet

---
+++ src/lab03_server.erl
@@ -0,0 +1,18 @@
+-module(lab03_server).
+
+%% lab03_server: lab03_server library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
